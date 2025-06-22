2a386929516be7146ec6489d92cc8be143599cf7
한태영 <dianspace53@gmail.com>
2025-06-22 19:05:27 +0900

    제스처 감지 시도중

--- Assets/VisionproTest/HandGestureTest/GestureToParticle.cs
+++ Assets/VisionproTest/HandGestureTest/GestureToParticle.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class GestureToParticle : MonoBehaviour
 {
@@ -11,86 +10,18 @@ public class GestureToParticle : MonoBehaviour
     
     private float lastTriggerTime;
     
-    // Input Actions
-    private InputAction selectAction;
-    private InputAction tapAction;
-    
-    void Start()
-    {
-        // PolySpatial Input Actions에서 Select 액션 찾기
-        var inputActions = FindObjectOfType<UnityEngine.InputSystem.PlayerInput>();
-        if (inputActions != null)
-        {
-            selectAction = inputActions.actions["Select"];
-            tapAction = inputActions.actions["Tap"];
-        }
-        
-        // Input Action 이벤트 등록
-        if (selectAction != null)
-        {
-            selectAction.performed += OnSelectPerformed;
-            selectAction.Enable();
-        }
-        
-        if (tapAction != null)
-        {
-            tapAction.performed += OnTapPerformed;
-            tapAction.Enable();
-        }
-    }
-    
-    void OnDestroy()
-    {
-        // Input Action 이벤트 해제
-        if (selectAction != null)
-        {
-            selectAction.performed -= OnSelectPerformed;
-            selectAction.Disable();
-        }
-        
-        if (tapAction != null)
-        {
-            tapAction.performed -= OnTapPerformed;
-            tapAction.Disable();
-        }
-    }
-    
     void Update()
     {
-        // 키보드 테스트용 (에디터에서)
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            TriggerParticle();
-        }
+        if (Time.time - lastTriggerTime < cooldownTime)
+            return;
         
-        // 마우스 테스트용 (에디터에서)
-        if (Input.GetMouseButtonDown(0))
+        // Vision Pro 터치 감지 (PolySpatial 호환)
+        if (Input.GetButtonDown("Submit") || 
+            Input.GetMouseButtonDown(0) ||
+            Input.GetKeyDown(KeyCode.Space))
         {
             TriggerParticle();
         }
-    }
-    
-    void OnSelectPerformed(InputAction.CallbackContext context)
-    {
-        // Vision Pro Select 제스처 (핀치)
-        if (CanTrigger())
-        {
-            TriggerParticle();
-        }
-    }
-    
-    void OnTapPerformed(InputAction.CallbackContext context)
-    {
-        // Vision Pro Tap 제스처
-        if (CanTrigger())
-        {
-            TriggerParticle();
-        }
-    }
-    
-    bool CanTrigger()
-    {
-        return Time.time - lastTriggerTime >= cooldownTime;
     }
     
     void TriggerParticle()
@@ -99,7 +30,7 @@ public class GestureToParticle : MonoBehaviour
         {
             testParticles.Play();
             lastTriggerTime = Time.time;
-            Debug.Log("Particle Triggered by Vision Pro Gesture!");
+            Debug.Log("Particle Triggered by Vision Pro Touch!");
         }
         else
         {
