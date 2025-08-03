f9a73cccd506b7196a3dd8002afd48f6b823d4f1
한태영 <dianspace53@gmail.com>
2025-08-03 17:01:15 +0900

    AI 서버 수정 사항 맞추어 웹소켓 수신 데이터 형식 수정

--- Assets/Scripts/Api/WebSocketVoiceClient.cs
+++ Assets/Scripts/Api/WebSocketVoiceClient.cs
@@ -58,18 +58,29 @@ public class WebSocketVoiceClient : MonoBehaviour
                     return;
                 }
 
-                var parsed = JsonConvert.DeserializeObject<AIMessage>(jsonString);
+                // var parsed = JsonConvert.DeserializeObject<AIMessage>(jsonString);
+                var parsed = JsonConvert.DeserializeObject<EmotionKeywordData>(jsonString);
 
-                if (parsed?.top_keywords != null && parsed.top_emotions != null)
-                {
-                    // if (AIResponseStore.Instance == null)
-                    // {
-                    //     Debug.Log("업성용");
-                    //     return;
-                    // }
-                    AIResponseStore.Instance?.UpdateData(parsed.top_keywords, parsed.top_emotions);
-                    Debug.Log($"[AI 요약 응답 수신]\n▶ Top Keywords: {string.Join(", ", parsed.top_keywords)}\n▶ Top Emotions: {string.Join(", ", parsed.top_emotions)}");
+                // if (parsed?.top_keywords != null && parsed.top_emotions != null)
+                // {
+                //     // if (AIResponseStore.Instance == null)
+                //     // {
+                //     //     Debug.Log("업성용");
+                //     //     return;
+                //     // }
+                //     AIResponseStore.Instance?.UpdateData(parsed.top_keywords, parsed.top_emotions);
+                //     Debug.Log($"[AI 요약 응답 수신]\n▶ Top Keywords: {string.Join(", ", parsed.top_keywords)}\n▶ Top Emotions: {string.Join(", ", parsed.top_emotions)}");
+                // }
+                if (parsed?.keywords != null && parsed.emotions != null)
+                { 
+                    AIResponseStore.Instance.UpdateData(parsed.keywords, parsed.emotions); 
+                    Debug.Log(
+                        $"[AI 요약]\n" + 
+                        $"▶ Keywords: {string.Join(", ", parsed.keywords)}\n" + 
+                        $"▶ Emotions: {string.Join(", ", parsed.emotions)}"
+                        );
                 }
+                
                 else
                 {
                     Debug.LogWarning("[Trigger WebSocket] 응답은 JSON이지만 요약 정보 없음.");
@@ -145,10 +156,10 @@ public class WebSocketVoiceClient : MonoBehaviour
     }
 }
 
-[Serializable]
-public class AIMessage
-{
-    public string type { get; set; }
-    public List<string> top_keywords { get; set; }
-    public List<string> top_emotions { get; set; }
-}
+// [Serializable]
+// public class AIMessage
+// {
+//     public string type { get; set; }
+//     public List<string> top_keywords { get; set; }
+//     public List<string> top_emotions { get; set; }
+// }
