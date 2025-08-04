4193d7a8fae6966cb35344bf89deeca6335adec9
한태영 <dianspace53@gmail.com>
2025-08-04 18:00:15 +0900

    ai 송신 rpc 부분 수정함

--- Assets/Scripts/AudienceReactDisplay/EmotionColorMapper.cs
+++ Assets/Scripts/AudienceReactDisplay/EmotionColorMapper.cs
@@ -8,12 +8,12 @@ public class EmotionColorMapper : MonoBehaviour
     public List<Color> emotionColors = new List<Color>(20);
 
     // 감정 이름 고정 배열 (Inspector 에디터에서 라벨로 사용)
-    public static readonly string[] emotionNames = new string[]
-    {
-        "사랑", "감사", "행복", "감동", "환희", "희열", "즐거움", "흥분",
-        "자부심", "만족", "기대감", "평온", "경외감", "열정", "몰입", "유대감",
-        "그리움", "설렘", "행복한 눈물", "충분함"
-    };
+    // public static readonly string[] emotionNames = new string[]
+    // {
+    //     "사랑", "감사", "행복", "감동", "환희", "희열", "즐거움", "흥분",
+    //     "자부심", "만족", "기대감", "평온", "경외감", "열정", "몰입", "유대감",
+    //     "그리움", "설렘", "행복한 눈물", "충분함"
+    // };
     
     /// <summary>
     /// 인덱스에 맞는 색상을 반환. 범위 벗어나면 흰색.
