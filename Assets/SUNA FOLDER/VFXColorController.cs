c1cbec6716be7bc16c9dfdb5cd366e551f623092
한태영 <dianspace53@gmail.com>
2025-08-07 14:35:03 +0900

    vfx 1초에 한번씩 색상 바뀌는 것으로 수정함

--- Assets/SUNA FOLDER/VFXColorController.cs
+++ Assets/SUNA FOLDER/VFXColorController.cs
@@ -1,17 +1,16 @@
 using UnityEngine;
 using UnityEngine.VFX;
+using System.Collections;
 
 public class VFXColorController : MonoBehaviour
 {
     public VisualEffect vfx;
     public Color colorA = Color.red;
     public Color colorB = Color.yellow;
-    
-    [Range(0.5f, 10f)]
-    public float intensityA = 10f;
 
-    [Range(0.5f, 10f)]
-    public float intensityB = 10f;
+    [Range(0.5f, 10f)] public float intensityA = 10f;
+
+    [Range(0.5f, 10f)] public float intensityB = 10f;
 
     void Start()
     {
@@ -20,22 +19,23 @@ public class VFXColorController : MonoBehaviour
             vfx.SetVector4("Color A", colorA * 8f);
             vfx.SetVector4("Color B", colorB * 8f);
         }
+
+        StartCoroutine(ChangeColorsRoutine());
     }
 
-    void Update()
+    IEnumerator ChangeColorsRoutine()
     {
-        // 키보드로 색상 랜덤 변경 (디버깅용)
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        while (true)
         {
+            yield return new WaitForSeconds(1f);
+
             colorA = Random.ColorHSV(0f, 1f, 1f, 1f, 1f, 1f);
             vfx.SetVector4("Color A", colorA * 8f);
-        }
 
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
+            yield return new WaitForSeconds(1f);
+
             colorB = Random.ColorHSV(0f, 1f, 1f, 1f, 1f, 1f);
             vfx.SetVector4("Color B", colorB * 8f);
         }
     }
 }
-
